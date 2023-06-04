dba1bef27c08bd5eb91110abdca6b8a495c47815
tiago <tiago7164@hotmail.com>
2023-06-05 00:27:22 +0100

    validação da função de cadastro e login

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PrisManager-CRM
-Projeto de CRM - TERA By - Anny Araujo - Tiago Luis - Patricia Eihara
+CRM Project Proposed By TERA - Realized By - Anny Araujo - Tiago Luis - Patricia Eihara
 
-## PRoject Description 
+## Project Description 
 <p align="justify"> The project consists of a CRM system for a tourism company. The system must be able to register packages, products and services, in addition to generating sales and customer reports. </p>
 
 ## Prerequisites
--- js/login.js
+++ js/login.js
@@ -1,3 +1,46 @@
+let usuarios = []
+
+function formCadastro() {
+
+  let email = document.querySelector("#cadInputEmail").value
+  let name = document.querySelector("#cadInputName").value
+  let password = document.querySelector("#cadInputPassword").value
+
+  let resultado = usuarios.find(usuario => usuario.email.password.name)
+
+  let user = {
+      name,
+      email,
+      password
+  }
+
+  usuarios.push(user)
+  console.log(usuarios);
+  alert(`Cadastrado com Sucesso!`)
+}
+
+function entrar() {
+
+  let email = document.querySelector("#loginInputEmail").value
+  let password = document.querySelector("#loginInputPassword").value
+
+  let resultado = usuarios.find(usuario => usuario.email === email)
+
+  if (resultado === undefined) {
+      alert("Email não cadastrado no nosso sistema")
+      return
+  }
+
+  if (resultado.password != password) {
+      alert("Email ou senha não conferem no sistema!")
+      return
+  }
+
+  alert(`Seja Bem Vindo ${resultado.name}`)
+}
+
+//
+
 const forms = document.querySelector(".forms"),
   pwShowHide = document.querySelectorAll(".eye-icon"),
   links = document.querySelectorAll(".link");
